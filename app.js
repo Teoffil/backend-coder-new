7f0fc6e62d0fd22c3479f14bf1f8da227fae921c
Teoffil <mateofileni1998@hotmail.com>
2024-02-26 18:34:18 -0300

    Avances en Resgitro de usuarios

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Importación de módulos y librerías
 const express = require('express');
 const { engine } = require('express-handlebars');
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
@@ -6,19 +7,34 @@ const http = require('http');
 const socketIo = require('socket.io');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
+// Importación de routers y modelos
 const productsRouter = require('./src/api/products/productsRouter');
 const cartsRouter = require('./src/api/carts/cartsRouter');
+const authRouter = require('./src/api/auth/authRouter');
 const Message = require('./src/dao/models/MessageSchema');
 const productManager = require('./src/dao/mongo/productManager');
+const User = require('./src/dao/models/UserSchema');
 
+// Configuración de la base de datos
 require('dotenv').config();
 const connectDB = require('./database');
 connectDB();
 
+// Creación de la aplicación Express y el servidor HTTP
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+
+// Configuración de la sesión
+app.use(session({
+    secret: 'mySecret', // Cambia esto por tu propia cadena secreta
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }) // Asegúrate de tener una variable de entorno MONGO_URI
+}));
 
 // Configuración de Handlebars
 app.engine('handlebars', engine({
@@ -27,18 +43,20 @@ app.engine('handlebars', engine({
 app.set('view engine', 'handlebars');
 app.set('views', './src/views');
 
-// Middleware para parsear JSON y cookies
+// Configuración del middleware para parsear JSON y cookies
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
-// Rutas estáticas
+// Configuración de las rutas estáticas
 app.use(express.static('public'));
 
-// Rutas de la API
+// Configuración de las rutas de la API
 app.use('/api/products', productsRouter);
 app.use('/carts', cartsRouter);
+app.use('/auth', authRouter);
 
-// Rutas de la aplicación
+// Configuración de las rutas de la aplicación
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -47,15 +65,27 @@ app.get('/chat', (req, res) => {
     res.render('chat');
 });
 
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
 app.get('/products', async (req, res) => {
     const { page = 1, limit = 10 } = req.query;
     try {
         const products = await productManager.getProducts({ page: parseInt(page), limit: parseInt(limit) });
-        // Obtén el ID del carrito de la cookie
-        const cartId = req.cookies.cartId;
+        const cartId = req.cookies.cartId; // Obtén el ID del carrito de la cookie
+
+        // Busca el usuario en la base de datos
+        const user = req.session.userId ? await User.findById(req.session.userId) : null;
+
         res.render('products', { 
             products: products.docs, 
-            cartId: cartId, // Asegúrate de pasar el cartId a la plantilla
+            cartId: cartId,
+            user: user, // Pasa el usuario a la vista
             prevPage: products.prevPage, 
             nextPage: products.nextPage,
             page,
---
+++ src/api/auth/authRouter.js
@@ -0,0 +1,59 @@
+// src/api/auth/authRouter.js
+const express = require('express');
+const User = require('../../dao/models/UserSchema'); // Asegúrate de ajustar la ruta al archivo UserSchema.js
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+    try {
+        const user = new User(req.body);
+
+        // Comprueba el correo electrónico y la contraseña antes de guardar el usuario
+        if (user.email === process.env.ADMIN_EMAIL && user.password === process.env.ADMIN_PASSWORD) {
+            user.role = 'admin';
+            req.session.role = 'admin';
+        } else {
+            req.session.role = 'usuario';
+        }
+
+        await user.save();
+        req.session.userId = user._id;
+        res.redirect('/products');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+
+router.post('/login', async (req, res) => {
+    try {
+        // Comprueba si las credenciales ingresadas son las del administrador
+        if (req.body.email === process.env.ADMIN_EMAIL && req.body.password === process.env.ADMIN_PASSWORD) {
+            req.session.role = 'admin';
+            return res.redirect('/products');
+        }
+
+        // Si no, procede con el inicio de sesión normal
+        const user = await User.findOne({ email: req.body.email });
+        if (!user || !user.comparePassword(req.body.password)) {
+            return res.status(400).send('Correo o contraseña incorrectos');
+        }
+        req.session.userId = user._id;
+        req.session.role = 'usuario';
+        res.redirect('/products');
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
+
+router.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.redirect('/products');
+        }
+        res.clearCookie('connect.sid');
+        res.redirect('/products');
+    });
+});
+
+module.exports = router;
